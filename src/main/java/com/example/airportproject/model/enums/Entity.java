9f1bcce9faea1e86e6f5f2536740ac050634295b
braym3 <ebray@qa.com>
2023-08-08 12:02:08 +0100

    added name attributes for entity types

--- src/main/java/com/example/airportproject/model/enums/Entity.java
+++ src/main/java/com/example/airportproject/model/enums/Entity.java
@@ -1,8 +1,18 @@
 package com.example.airportproject.model.enums;
 
 public enum Entity {
-    FLIGHT,
-    GATE,
-    TERMINAL,
-    RUNWAY
+    FLIGHT("Flight"),
+    GATE("Gate"),
+    TERMINAL("Terminal"),
+    RUNWAY("Runway");
+
+    private String name;
+
+    Entity(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
